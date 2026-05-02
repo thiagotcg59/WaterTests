c079c1c637fe5f8cd5e6b7c0e9be64985633dc91
Thiago <thiagotcg59@users.noreply.github.com>
2026-05-02 15:58:04 -0400

    fix: apply basePath only in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+const repoBasePath = '/WaterTests';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/WaterTests',
+  basePath: isProduction ? repoBasePath : undefined,
+  assetPrefix: isProduction ? `${repoBasePath}/` : undefined,
 };
 
 export default nextConfig;
